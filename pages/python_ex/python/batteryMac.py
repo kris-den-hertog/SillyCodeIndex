db67509119da5a28a8304bf1d47c94a2ef3cffe5
kris-den-hertog <122313839+kris-den-hertog@users.noreply.github.com>
2024-03-22 11:32:48 +0100

    battery checker added

--- pages/python_ex/python/batteryMac.py
+++ pages/python_ex/python/batteryMac.py
@@ -1,25 +1,23 @@
-
 # make sure you have installed psutil via pip3 install psutil!
 import psutil
 import time
 import subprocess
-                    
+
 # Function to display notification on macOS
 def notify_mac(title, message):
     subprocess.call(['osascript', '-e', f'display notification "{message}" with title "{title}"'])
-                    
+
 # Loop that checks if battery needs charging
 while True:
     battery = psutil.sensors_battery()
     percent = battery.percent
     low = 10
-                    
+
     if percent > low:
-        notify_mac("Battery", f"No charging needed! {percent}% Battery remaining.")
-                    
+            notify_mac("Battery", f"No charging needed! {percent}% Battery remaining.")
+
     else:
-        notify_mac("Battery", f"Please charge, {percent}% Battery remaining.")
-                    
-    # notify user every 60 min
+         notify_mac("Battery", f"Please charge, {percent}% Battery remaining.")
+
+ # notify user every 60 min
     time.sleep(60 * 60)
-    
